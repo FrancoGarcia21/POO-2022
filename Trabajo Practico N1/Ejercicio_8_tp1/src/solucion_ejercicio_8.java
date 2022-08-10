a28b18ba111bc6cfda15e6cbf92b7f8ffe59c5d6
FrancoGarcia21 <110432909+FrancoGarcia21@users.noreply.github.com>
2022-08-09 23:52:46 -0300

    Ejercicios nuevos hechos

--- Trabajo Practico N1/Ejercicio_8_tp1/src/solucion_ejercicio_8.java
+++ Trabajo Practico N1/Ejercicio_8_tp1/src/solucion_ejercicio_8.java
@@ -1,9 +1,28 @@
-
+import java.security.SecureRandom;
+import java.util.Scanner;
 public class solucion_ejercicio_8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner in = new Scanner(System.in);
+	System.out.println("Ingrese un numero entero");
+	Integer numero = in.nextInt();
+	SecureRandom random = new SecureRandom();
+	Integer aleatorio = random.nextInt(10);
+	if (numero.equals(aleatorio)) 
+	{
+		System.out.println("El numero ingresado que fue " +numero+ " es igual al numero que se genero aleatoriamente " +aleatorio);
+	} else 
+	{
+		System.out.println("El numero que ingreso " +numero+" no es igual al numero que se genero aleatoriamente que fue " +aleatorio);
 	}
-
+	if(numero>= aleatorio)
+	{
+	System.out.println("El numero que usted ingreso que fue "+numero+ " es mayor o igual que el numero que se genero que fue "+ aleatorio);
+	}
+//	switch (numero) {
+//	case numero > aleatorio : System.out.println("El numero ingreso que fue "+numero+ " es mayor que el numero aleatorio generado "+aleatorio); break;
+//	case numero >= aleatorio: System.out.println("El numero que ingreso fue " +numero+ " es menor o igual al numero que se genero aleatoriamente"+aleatorio); break;
+//	}
+	
+	}
 }
---
+++ Trabajo Practico N1/Ejercicio_9_tp1/src/solucion_ejercicio_9.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class solucion_ejercicio_9 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		System.out.println(" Ingrese una palabra");
+		String cade1 = in.next();
+		System.out.println(" Ingresa otra palabra ");
+		String cade2 = in.next();
+		
+		if (cade1.equals(cade2)){
+			
+		}
+
+	}
+
+}
